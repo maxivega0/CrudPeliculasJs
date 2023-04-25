922843b8165706142646644a596bc4fc35655adf
maxivega0 <maximilianovega600@gmail.com>
2023-04-25 17:42:21 -0300

    agregado funcion validar formulario

--- js/administrador.js
+++ js/administrador.js
@@ -11,6 +11,7 @@
 
 
 import Pelicula from "./clasePelicula.js";
+import { resumenValidaciones, } from "./helpers.js"
 
 
 // variables globales
@@ -53,20 +54,25 @@ import Pelicula from "./clasePelicula.js";
 
   function crearPelicula() {
     // Validar datos
-
-    // Si los datos son validos
+const resumen = resumenValidaciones(titulo.value);
+if (resumen.length === 0) {
+      // Si los datos son validos
     // Creo el objeto Pelicula
     const peliculaEjemplo = new Pelicula(
-        "0001",
-        "El Padrino",
-        "La familia Corleone es una de las más poderosas de Nueva York en los años 40.",
-        "https://imagenes.psicologiaymente.com/wp-content/uploads/2021/02/el-padrino-1.png",
-        "Drama/Crimen",
-        1972,
-        "2h 55min",
-        "Estados Unidos",
-        ["Marlon Brando", "Al Pacino", "James Caan"]
-      );
+      "0001",
+      "El Padrino",
+      "La familia Corleone es una de las más poderosas de Nueva York en los años 40.",
+      "https://imagenes.psicologiaymente.com/wp-content/uploads/2021/02/el-padrino-1.png",
+      "Drama/Crimen",
+      1972,
+      "2h 55min",
+      "Estados Unidos",
+      ["Marlon Brando", "Al Pacino", "James Caan"]
+    );
+    console.log(peliculaEjemplo);
+}else{
+  console.log("Aqui ocurrieron errores que tengo que mostrar");
+}
 
       //* Agregar el objeto en el array de peliculas
       listaPelicula.push();
