b303baff79aee5bb23041c1b05f9dbe72ad88ce3
maxivega0 <maximilianovega600@gmail.com>
2023-05-09 18:05:16 -0300

    correccion en comillas del boton para acceder a la pagina detalle

--- js/indexPelicula.js
+++ js/indexPelicula.js
@@ -13,7 +13,7 @@ function mostrarPeliculaIndex(pelicula) {
       <img src=" ${pelicula.imagen} " class="card-img-top h-75 img-fluid" alt=" ${pelicula.titulo}">
       <div class="card-body d-flex flex-column justify-content-between">
         <h5 class="card-title">${pelicula.titulo}</h5>
-        <button class="btn btn-outline-primary" onclick="navegarPaginaDetalle("${pelicula.codigo}")">Ver detalle</button>
+        <button class="btn btn-outline-primary" onclick="navegarPaginaDetalle('${pelicula.codigo}')">Ver detalle</button>
       </div>
     </div>
   </div>
@@ -26,5 +26,5 @@ function navegarPaginaDetalle(codigo) {
   // Nombre de dominio/pages/detalle.html
   console.log(window.location+'/pages/detalle.html');
   console.log(window.location.origin+"/pages/detalle.html");
-  window.location.href = window.location.origin+'/pages/detalle.html';
+  window.location.href = window.location.origin+'/pages/detalle.html?codigo='+codigo;
 }
